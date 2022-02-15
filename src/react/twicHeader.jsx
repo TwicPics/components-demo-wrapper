3618b5f94c001318f336daba9e56694bfe29f5e2
Eloïse <eloise-martin@live.fr>
2022-02-15 14:30:54 +0100

    Fixes rebase conflicts (linter install)

--- src/react/twicHeader.jsx
+++ src/react/twicHeader.jsx
@@ -1,57 +1,54 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 
-const TwicHeader = ({
-  title = "",
-  tags = [],
-  redirect = `https://www.twicpics.com/docs/getting-started/overview`,
-}) => {
-  return (
+const TwicHeader = ( { title = ``,
+    tags = [],
+    redirect = `https://www.twicpics.com/docs/getting-started/overview` } ) => (
     <header id="twic-header">
-      <h1 className="heading-primary">
-        <img
-          className="twicpics-logo"
-          src="https://assets.website-files.com/5ec5244ea1611d6855cdc471/615dc545a7fa485a4df0ea0a_Horizontal%403x.png"
-          alt="TwicPics logo"
-        />
-        <span>x</span>
-        <img
-          className="react-logo"
-          src="https://assets.twicpics.com/demo/@twicpics-components/framework-logos/react.png"
-          alt="React logo"
-        />
-      </h1>
-      <h2 className="heading-secondary">{title}</h2>
-      <div className="tags-container">
-        {tags.map((tag, i) => (
-          <div key={i} className="tag">
-            {tag}
-          </div>
-        ))}
-        <a
-          href={redirect}
-          target="_blank"
-          className="documentation-link tag tag--bg-purple-rain"
-        >
-          TwicPics documentation
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="documentation-link-icon"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M17 8l4 4m0 0l-4 4m4-4H3"
+        <h1 className="heading-primary">
+            <img
+                className="twicpics-logo"
+                // eslint-disable-next-line max-len
+                src="https://assets.website-files.com/5ec5244ea1611d6855cdc471/615dc545a7fa485a4df0ea0a_Horizontal%403x.png"
+                alt="TwicPics logo"
             />
-          </svg>
-        </a>
-      </div>
+            <span>x</span>
+            <img
+                className="react-logo"
+                src="https://assets.twicpics.com/demo/@twicpics-components/framework-logos/react.png"
+                alt="React logo"
+            />
+        </h1>
+        <h2 className="heading-secondary">{title}</h2>
+        <div className="tags-container">
+            {tags.map( ( tag, i ) => (
+                <div key={i} className="tag">
+                    {tag}
+                </div>
+            ) )}
+            <a
+                href={redirect}
+                target="_blank"
+                className="documentation-link tag tag--bg-purple-rain" rel="noreferrer"
+            >
+          TwicPics documentation
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="documentation-link-icon"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    />
+                </svg>
+            </a>
+        </div>
     </header>
-  );
-};
+);
 
 export default TwicHeader;
--- src/react/twicWrapper.jsx
+++ src/react/twicWrapper.jsx
@@ -4,21 +4,21 @@ import { default as TwicHeader } from "./twicHeader";
 import React, { useEffect } from "react";
 import WebFont from "webfontloader";
 
-const TwicWrapper = ({ title, tags, redirect, children }) => {
-  useEffect(() => {
-    WebFont.load({
-      google: {
-        families: ["Montserrat:400,600,700"],
-      },
-    });
-  }, []);
+const TwicWrapper = ( { title, tags, redirect, children } ) => {
+    useEffect( () => {
+        WebFont.load( {
+            "google": {
+                "families": [ `Montserrat:400,600,700` ],
+            },
+        } );
+    }, [] );
 
-  return (
-    <div id="twic-demo-wrapper">
-      <TwicHeader title={title} tags={tags} redirect={redirect}></TwicHeader>
-      <main id="twic-demo-container">{children}</main>
-    </div>
-  );
+    return (
+        <div id="twic-demo-wrapper">
+            <TwicHeader title={title} tags={tags} redirect={redirect}></TwicHeader>
+            <main id="twic-demo-container">{children}</main>
+        </div>
+    );
 };
 
 export default TwicWrapper;
