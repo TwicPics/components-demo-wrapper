8de07ad284499433542612cedffda2ddd8d89dc9
Miguel Beignon <miguel@twic.pics>
2022-03-07 23:53:40 +0100

    chores : React Home page design refactoring

--- samples/react/src/components/_home/index.jsx
+++ samples/react/src/components/_home/index.jsx
@@ -4,79 +4,86 @@ import { NavLink } from "react-router-dom";
 import { TwicImg } from "@twicpics/components/react";
 import "./index.scss";
 
-const componentProperties = [
+const categories = [
     {
-        "route": `/focus`,
-        "imgSrc": `components/puppy.jpg`,
-        "title": `Focus`,
+        "title": `Use cases`,
+        "items": [
+            {
+                "route": `/focus`,
+                "imgSrc": `components/puppy.jpg`,
+                "title": `Focus`,
+            },
+            {
+                "route": `/mode`,
+                "imgSrc": `components/fox.jpg`,
+                "title": `Mode`,
+            },
+            {
+                "route": `/placeholders`,
+                "imgSrc": `components/cat.jpg`,
+                "title": `Placeholders`,
+            },
+            {
+                "route": `/positions`,
+                "imgSrc": `components/position/horse.jpg`,
+                "title": `Positions`,
+            },
+            {
+                "route": `/ratio`,
+                "imgSrc": `components/woman-and-winter.jpg`,
+                "title": `Ratio`,
+            },
+            {
+                "route": `/transition`,
+                "imgSrc": `components/peacock.jpg`,
+                "title": `Transition`,
+            },
+        ],
     },
     {
-        "route": `/mode`,
-        "imgSrc": `components/fox.jpg`,
-        "title": `Mode`,
-    },
-    {
-        "route": `/placeholders`,
-        "imgSrc": `components/cat.jpg`,
-        "title": `Placeholders`,
-    },
-    {
-        "route": `/positions`,
-        "imgSrc": `components/position/horse.jpg`,
-        "title": `Positions`,
-    },
-    {
-        "route": `/ratio`,
-        "imgSrc": `components/woman-and-winter.jpg`,
-        "title": `Ratio`,
-    },
-    {
-        "route": `/transition`,
-        "imgSrc": `components/peacock.jpg`,
-        "title": `Transition`,
-    },
-];
-
-const useCases = [
-    {
-        "route": `/basic-grid`,
-        "imgSrc": `https://assets.twicpics.com/examples/football.jpg`,
-        "title": `Basic Example`,
-    },
-    {
-        "route": `/art-direction`,
-        "imgSrc": `components/greece.jpg`,
-        "title": `Art direction`,
-    },
-    {
-        "route": `/flip`,
-        "imgSrc": `components/flip/orange-1.jpg`,
-        "title": `Flip Card`,
-    },
-    {
-        "route": `/life-cycle`,
-        "imgSrc": `components/portraits/woman-1.jpg`,
-        "title": `Life cycle`,
-    },
-    {
-        "route": `/modal`,
-        "imgSrc": `components/modal/seagull.jpg`,
-        "title": `Modal`,
-    },
-    {
-        "route": `/slider`,
-        "imgSrc": `components/slider/pantone.jpg`,
-        "title": `Slider`,
-    },
-    {
-        "route": `/slideshow`,
-        "imgSrc": `components/slideshow/space-1.jpg`,
-        "title": `Slideshow`,
-    },
-    {
-        "route": `/style-driven`,
-        "imgSrc": `components/horse.jpg`,
-        "title": `Style Driven`,
+        "title": `Components properties`,
+        "items": [
+            {
+                "route": `/basic-grid`,
+                "imgSrc": `https://assets.twicpics.com/examples/football.jpg`,
+                "title": `Basic Example`,
+            },
+            {
+                "route": `/art-direction`,
+                "imgSrc": `components/greece.jpg`,
+                "title": `Art direction`,
+            },
+            {
+                "route": `/flip`,
+                "imgSrc": `components/flip/orange-1.jpg`,
+                "title": `Flip Card`,
+            },
+            {
+                "route": `/life-cycle`,
+                "imgSrc": `components/portraits/woman-1.jpg`,
+                "title": `Life cycle`,
+            },
+            {
+                "route": `/modal`,
+                "imgSrc": `components/modal/seagull.jpg`,
+                "title": `Modal`,
+            },
+            {
+                "route": `/slider`,
+                "imgSrc": `components/slider/pantone.jpg`,
+                "title": `Slider`,
+            },
+            {
+                "route": `/slideshow`,
+                "imgSrc": `components/slideshow/space-1.jpg`,
+                "title": `Slideshow`,
+            },
+            {
+                "route": `/style-driven`,
+                "imgSrc": `components/horse.jpg`,
+                "title": `Style Driven`,
+            },
+        ],
     },
 ];
 
@@ -84,44 +91,27 @@ function Home() {
     return (
         <TwicWrapper>
             <div className="home-container">
-                <div className="twic-panel">
-                    <h3>Use cases</h3>
-                    <div className="twic-grid">
-                        { useCases.map( ( item, i ) => (
-                            <NavLink to={ item.route } key={`useCase ${ i }`}>
-                                <figure className="twic-item">
-                                    <TwicImg
-                                        src={ item.imgSrc }
-                                        focus="auto"
-                                        ratio="0.95">
-                                    </TwicImg>
-                                    <figcaption>
-                                        <p>{ item.title }</p>
-                                    </figcaption>
-                                </figure>
-                            </NavLink>
-                        ) ) }
+                { categories.map( ( category, i ) => (
+                    <div className="twic-panel" key={`category ${ i }`}>
+                        <h3>{ category.title }</h3>
+                        <div className="twic-grid">
+                            { category.items.map( ( item, i ) => (
+                                <NavLink to={ item.route } key={`useCase ${ i }`}>
+                                    <figure className="twic-item">
+                                        <TwicImg
+                                            src={ item.imgSrc }
+                                            focus="auto"
+                                            ratio="0.95">
+                                        </TwicImg>
+                                        <figcaption>
+                                            <p>{ item.title }</p>
+                                        </figcaption>
+                                    </figure>
+                                </NavLink>
+                            ) ) }
+                        </div>
                     </div>
-                </div>
-                <div className="twic-panel">
-                    <h3>Components properties</h3>
-                    <div className="twic-grid">
-                        { componentProperties.map( ( item, i ) => (
-                            <NavLink to={ item.route } key={`property ${ i }`}>
-                                <figure className="twic-item">
-                                    <TwicImg
-                                        src={ item.imgSrc }
-                                        focus="auto"
-                                        ratio="0.95">
-                                    </TwicImg>
-                                    <figcaption>
-                                        <p>{ item.title }</p>
-                                    </figcaption>
-                                </figure>
-                            </NavLink>
-                        ) ) }
-                    </div>
-                </div>
+                ) )}
             </div>
         </TwicWrapper>
     );
