4a71fbeb866b112cd98abda8d5a8e5f3a2ae8063
Miguel Beignon <miguel@twic.pics>
2022-03-18 21:35:04 +0100

    adds gatsby basicGrid && art Direction

---
+++ samples/gatsby/src/pages/twicArtDirection/index.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import {
+    twicArtDirectionContainer,
+    styleDrivenResponsive,
+} from "./index.module.scss";
+import { TwicWrapper, TwicAbstract } from "@twicpics/components-demo-wrapper/react";
+import { TwicImg } from "@twicpics/components/react";
+
+const imgUrl = `components/greece.jpg`;
+
+/**
+ * demonstrates that setting up components using CSS enables hassle-free responsive designs.
+ */
+const TwicArtDirection = () => (
+    <TwicWrapper framework="gatsby">
+        <div className={ twicArtDirectionContainer }>
+            <TwicAbstract
+                title="Art Direction with CSS Media Queries"
+                codeSandUrl="https://codesandbox.io/s/twicpics-x-next-art-direction-ps2zj"
+                codeSandBoxName="TwicPics x Next - Art Direction"
+            >
+                <p>Setting up components using CSS enables hassle-free responsive designs.</p>
+                <p>You do not need to create a set of images derived from the original.</p>
+                <p>
+                    You only need to manage a master image in your template:
+                    TwicPics understands your CSS and does the rest.
+                </p>
+                <p>
+                    <strong>Change the width of your browser </strong>
+                    to see the image ratio adapt according to the CSS rules.
+                </p>
+            </TwicAbstract>
+            <div className={ `${ styleDrivenResponsive } twic-item }`}>
+                <TwicImg src={imgUrl} className={ styleDrivenResponsive } focus="48px100p">
+                </TwicImg>
+            </div>
+        </div>
+    </TwicWrapper>
+);
+
+export default TwicArtDirection;
---
+++ samples/gatsby/src/pages/twicBasicGrid/index.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { twicBasicGridContainer } from "./index.module.scss";
+import { TwicWrapper, TwicAbstract } from "@twicpics/components-demo-wrapper/react";
+import { TwicImg } from "@twicpics/components/react";
+
+function TwicBasicGrid() {
+    return (
+        <TwicWrapper framework="gatsby">
+            <div className={ twicBasicGridContainer }>
+                <TwicAbstract
+                    title="A basic example"
+                    codeSandUrl="https://codesandbox.io/s/twicpics-x-next-basic-example-xe9spg"
+                    codeSandBoxName="TwicPics x Next - A basic example"
+                >
+                    <p>
+                        TwicPics is a Responsive Image Service Solution <span className="twic-code">SaaS</span>
+                        that enables on-demand responsive image generation.
+                    </p>
+                    <p>
+                        TwicPics Components make it dead easy to unleash the power of TwicPics.
+                    </p>
+
+                        Here the properties used in this example :
+                    <ul>
+                        <li>
+                            <span className="twic-code">ratio</span>:
+                            <span>
+                                    determines the value of the width/height ratio
+                                    of the image display area.</span>
+                        </li>
+                        <li>
+                            <span className="twic-code">mode</span>:
+                            <span>determines if the image fills or sits inside the area.</span>
+                        </li>
+                        <li>
+                            <span className="twic-code">focus</span>:
+                            <span>changes the focus point coordinates of the image.</span>
+                        </li>
+                        <li>
+                            <span className="twic-code">placeholder</span>:
+                            <span>helps optimize your CLS and implement the LQIP technique.</span>
+                        </li>
+                    </ul>
+                </TwicAbstract>
+                <div className="twic-grid">
+                    <div className="twic-item">
+                        <TwicImg src="https://assets.twicpics.com/examples/football.jpg" />
+                        <span>
+                            default
+                        </span>
+                    </div>
+                    <div className="twic-item">
+                        <TwicImg
+                            src="/football.jpg"
+                            ratio="16/9"
+                            mode="cover"
+                            placeholder="meancolor"
+                        />
+                        <span>
+                             ratio=16/9, image fills area
+                        </span>
+                    </div>
+                    <div className="twic-item">
+                        <TwicImg
+                            src="/football.jpg"
+                            step="100"
+                            mode="contain"
+                            ratio="16/9"
+                        />
+                        <span>
+                             ratio=16/9, image sits inside area
+                        </span>
+                    </div>
+                    <div className="twic-item">
+                        <TwicImg
+                            src="/football.jpg"
+                            step="100"
+                            focus="auto"
+                            ratio="2/3"
+                            placeholder="maincolor"
+                        />
+                        <span>
+                             ratio=2/3, image fills area + focus auto
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </TwicWrapper>
+    );
+}
+export default TwicBasicGrid;
