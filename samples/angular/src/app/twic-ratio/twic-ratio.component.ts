7997988dd86653d8572f689cf5274d88ed4afa0f
Miguel Beignon <miguel@twic.pics>
2022-04-29 09:43:52 +0200

    Modifications following correction of the refresh issue in angular (component 0.6.6)

--- samples/angular/src/app/twic-ratio/twic-ratio.component.ts
+++ samples/angular/src/app/twic-ratio/twic-ratio.component.ts
@@ -10,17 +10,13 @@ export class TwicRatioComponent implements OnInit {
   ratioValues = [`2.39`, `16/9`, `4/3`, ``, `0.75`];
   ratioIndex = 0;
   fullWidthAera = true;
-  // trick to force reload TwicImg
-  show = true;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   onChangeRatio() {
-    setTimeout(() => (this.show = false));
     this.ratioIndex = (this.ratioIndex + 1) % this.ratioValues.length;
-    setTimeout(() => (this.show = true));
   }
 
   onChangeAreaWidth() {
