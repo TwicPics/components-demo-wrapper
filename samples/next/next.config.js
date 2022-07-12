ca1ca0a242b6755b92ef4ef3df944b8076ab6a39
Eloïse <eloise-martin@live.fr>
2022-07-12 13:45:40 +0200

    Updates Next dependencies

--- samples/next/next.config.js
+++ samples/next/next.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable strict */
 /* globals module, require */
 "use strict";
-
-const withCompileNodeModules = require( `@moxy/next-compile-node-modules` );
-
-module.exports = withCompileNodeModules( {
-    "test": /\.jsx$/,
-} );
+const withTM = require(`next-transpile-modules`)([
+  `@twicpics/components-demo-wrapper`,
+]);
+module.exports = withTM({
+  test: /\.jsx$/,
+});
